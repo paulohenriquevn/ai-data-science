8ad321a6ee316f769d53bec981ed5d5582901a37
paulohenriquevn <paulohenriquevn@gmail.com>
2025-03-22 12:43:41 -0300

    Organizando projeto

--- src/feature_engineer/feature_engineer_step.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-import pandas as pd
-
-
-class FeatureEngineerStep(ABC):
-    @abstractmethod
-    def transform(self, data: pd.DataFrame) -> pd.DataFrame:
-        pass
